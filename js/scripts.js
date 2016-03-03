24f5e92252b1ec9d367ed92ed32f56406da38a42
lissah <nmastudent@KOPMAC006.local>
2016-03-03 09:49:20 -1000

    added jQuery toggle

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-console.log('Hello World')
+var $navToggle =  $('.nav-toggle');
+var $mainNavUl = $('.main-nav ul');
+var $window = $(window);
+
+$navToggle.on('click', function() {
+$mainNavUl.toggle();
+$navToggle.toggleClass('active');
+});
+
+$(window).on('resize', function(){
+  $mainNavUl.show();
+  if ($window.width() >767) {
+  $mainNavUl.show();
+  $navToggle.addClass('active');
+}
+})
